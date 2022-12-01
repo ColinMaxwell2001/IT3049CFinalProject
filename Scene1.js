fef9743b93272f8c3a12541f56a7b90ea88825f9
grantkollar <89742140+grantkollar@users.noreply.github.com>
2022-12-01 14:07:16 -0500

    New background

--- Scene1.js
+++ Scene1.js
@@ -4,7 +4,7 @@ class Scene1 extends Phaser.Scene {
     }
 
     preload(){
-        this.load.image("background", "assets/images/background.png");
+        this.load.image("background", "assets/images/back.png");
         //this.load.image("ship", "assets/images/ship.png");
         //this.load.image("ship2", "assets/images/ship2.png");
         //this.load.image("ship3", "assets/images/ship3.png");
