58c5bf25a548e1b0a1b8e7b2a74c4f1d8f84d574
grantkollar <89742140+grantkollar@users.noreply.github.com>
2022-12-07 13:26:24 -0500

    last touches

--- Scene2.js
+++ Scene2.js
@@ -2,7 +2,7 @@ class Scene2 extends Phaser.Scene{ //Here!!
     constructor(){
         super("playGame");
     }
-
+    //initiliaztion
     create(){
         
         //this.background = this.add.image(0, 0,"background");
@@ -23,47 +23,9 @@ class Scene2 extends Phaser.Scene{ //Here!!
         document.body.webkitRequestFullScreen();
         this.enterButtonHoverState();
     });
-        //this.ship1 = this.add.image(config.width/2 -50, config.height/2, "ship");
-        //this.ship2 = this.add.image(config.width/2, config.height/2, "ship2");
-        //this.ship3 = this.add.image(config.width/2 +50, config.height/2, "ship3");
 
-        // this.ship1 = this.add.sprite(config.width / 2 - 50, config.height / 2, "ship");
-        // this.ship2 = this.add.sprite(config.width / 2, config.height / 2, "ship2");
-        // this.ship3 = this.add.sprite(config.width / 2 + 50, config.height / 2, "ship3");
-/*
-        this.anima.create({
-            key:"ship1_anim",
-            frames:this.anims.generateFrameNumbers("ship"),
-            frameRate:20,
-            repeat:-1
-        });
-
-        this.anima.create({
-            key:"ship2_anim",
-            frames:this.anims.generateFrameNumbers("ship2"),
-            frameRate:20,
-            repeat:-1
-        });
-
-        this.anima.create({
-            key:"ship2_anim",
-            frames:this.anims.generateFrameNumbers("ship3"),
-            frameRate:20,
-            repeat:-1
-        });
-
-        this.anima.create({
-            key:"explode",
-            frames:this.anims.generateFrameNumbers("explosion"),
-            frameRate:20,
-            repeat:0,
-            hideOnComplete:true
-        });
-*/
         this.enemies = this.physics.add.group();
-        // this.enemies.add(this.ship1);
-        // this.enemies.add(this.ship2);
-        // this.enemies.add(this.ship3);
+
         this.shipDirection = true;
         
         this.shipGrave = [];
@@ -98,6 +60,7 @@ class Scene2 extends Phaser.Scene{ //Here!!
             }
         }, this.enemyFireRate);
 
+        //power up spawner
         clearInterval(this.spawnPowerUp);
         this.spawnPowerUp = setInterval(() => {
             var powerUp = this.physics.add.sprite(16, 16, "power-up");
@@ -115,7 +78,11 @@ class Scene2 extends Phaser.Scene{ //Here!!
             powerUp.setCollideWorldBounds(true);
             powerUp.setBounce(1);
         }, 20000);
+
         this.powerUpActivite = false;
+        
+
+        //adding ships to the enemy
         this.shipsArr1.forEach(element => {
             element.play("ship1_anim");
             this.enemies.add(element);
@@ -129,28 +96,26 @@ class Scene2 extends Phaser.Scene{ //Here!!
             this.enemies.add(element);
         })
 
-
-        // this.ship1.play("ship1_anim");
-        // this.ship2.play("ship2_anim");
-        // this.ship3.play("ship3_anim");
-
-        // this.ship1.setInteractive();
-        // this.ship2.setInteractive();
-        // this.ship3.setInteractive();
-
         this.input.on('gameobjectdown', this.destroyShip, this);
         this.fireInterval = 700;
         
         this.powerUps = this.physics.add.group();
 
         var maxObjects = 0;
+
+        //bonus ship spawner
         this.bonusShipActive = false;
+        clearInterval(this.bonusShipSpawn);
         this.bonusShipSpawn = setInterval(() => {
+            console.log("spawning bonus");
             this.bonusship = this.add.sprite(config.width-100,config.height-600,"bonusship");
             this.enemies.add(this.bonusship);
             this.bonusShipActive = true;
         }, Math.floor(Math.random() * 20000) + 30000);
 
+
+
+            //power up spawner
           var powerUp = this.physics.add.sprite(16, 16, "power-up");
           this.powerUps.add(powerUp);
           powerUp.setRandomPosition(0, 0, config.width, config.height);     /* Had to take out the keyword "game" in game.config in order to make work */
@@ -186,7 +151,7 @@ class Scene2 extends Phaser.Scene{ //Here!!
         this.physics.add.overlap(this.projectiles, this.enemies, this.hitEnemy, null, this);
 
         this.physics.add.overlap(this.enemyProjectiles,this.player,this.hitPlayer,null,this);
-        //Tutorial #9
+
 
         var graphics = this.add.graphics();
         graphics.fillStyle(0x000000, 1);
@@ -233,7 +198,7 @@ class Scene2 extends Phaser.Scene{ //Here!!
     }
 
     
-
+    //power picker
     pickPowerUp(player, powerUp) {
         
         this.randNum = Math.random();
@@ -273,6 +238,7 @@ class Scene2 extends Phaser.Scene{ //Here!!
             setTimeout(() => {
                 this.doubleshot = false;
                 this.powerUpActivite = false;
+                this.fireInterval = 700;
             }, 5000);
             
             
@@ -307,17 +273,17 @@ class Scene2 extends Phaser.Scene{ //Here!!
                 this.powerUpIdentifier.text = ""; //Remove Display
             }, 1500);
 
-            this.fireInterval = 300
+            this.fireInterval = 200
 
             setTimeout(() => {
-                this.fireInterval = 1000;
+                this.fireInterval = 700;
                 this.powerUpActivite = false;
             }, 5000);
         }
 
         powerUp.disableBody(true, true);
     }
-
+    //enemy getting hit by ship
     hurtPlayer(player, enemy) {
         this.resetShipPos(enemy);
         this.playerDied = true;
@@ -338,33 +304,14 @@ class Scene2 extends Phaser.Scene{ //Here!!
 
 
     }
-
-    resetPlayer() {
-        var x = config.width / 2 - 8;
-        var y = config.height + 64;
-        this.player.enableBody(true, x, y, true, true);
-        this.player.alpha = 0.5;
-
-        var tween = this.tweens.add({
-            targets: this.player,
-            y: config.height - 64, 
-            ease: 'Power1',
-            duration: 1500, 
-            repeat: 0,
-            onComplete: function() {
-                this.player.alpha = 1;
-            },
-            callbackScope: this
-        });
-    }
-
+    //player getting hit my enemy projectile
     hitPlayer(proj,player){
         let explosion = new Explosion(this,player.x,player.y);
         this.playerDied = true;
         player.disableBody(true,true);
         proj.destroy();
     }
-
+    //hitting the enemy with player projectile
     hitEnemy(projectile, enemy) {
         // console.log("enemies hit : " + this.enemiesHit);
         var explosion = new Explosion(this, enemy.x, enemy.y);
@@ -396,48 +343,8 @@ class Scene2 extends Phaser.Scene{ //Here!!
         var scoreFormated = this.zeroPad(this.score, 6);
         this.scoreLabel.text = "SCORE " + scoreFormated;
     }
-    // moveShip(ship, speed) {
-    //     if(this.shipDirection){
-    //         ship.x += speed;
-    //     }
-    //     else{
-    //         ship.x -= speed;
-    //     }
-
-
-    //     if ((ship.x > (config.width - 100)) && this.shipDirection)
-    //     {
-    //         //this.resetShipPos(ship);
-    //         this.reverseShipMovement();
-    //         this.shipDirection = false; 
-    //     }
-    //     else if(ship.x < (100) && !this.shipDirection){
-    //         this.shipDirection = true;
-    //     }
-
-    // }
-
-
-    
-
-    // reverseShipMovement(){
-    //     this.shipsArr1.forEach(element => {
-    //         this.moveShip(element,-.5);
-    //     });
-    //     this.shipsArr2.forEach(element=>{
-    //         this.moveShip(element,-.5);
-    //     })
-    //     this.shipsArr3.forEach(element=>{
-    //         this.moveShip(element,-.5);
-    //     });
-    // }
-
-
-
-
+    //moves all the ships left to right
     moveAllShips(speed){
-        // console.log(this.shipsArr1[0].x)
-        // console.log(this.shipsArr1[0].y);
         if(this.shipDirection){
         
         for(let i = 0; i < this.shipsArr1.length; i++){
@@ -477,26 +384,12 @@ class Scene2 extends Phaser.Scene{ //Here!!
             }
         }
         
-        // console.log(this.shipsArr1[0] >= config.width)
-        // console.log(this.shipDirection)
-       
-        // if(shipArr[0] < 100){
-        //     shipArr.forEach(ship => {
-        //         ship.x += speed;
-        //     })
-        // }
     }
-
+    //respawns ships and makes them slightly faster each time all the ships are destroyed on the screen
    newLevel(){
     this.level++;
     this.levelLabel.text = "Level " + this.level;
 
-
-    /*this.levelScreen = this.add.bitmapText(100, 300, 'pixelfont', 'Level ' + this.level, 16);
-    this.levelScreen.text = "Level " + this.level;*/
-    
-    //increase enemy fire rate
-    //increase enemy speed
     this.velocity = this.velocity + .1;
 
     //reset deadShipCount
@@ -570,7 +463,7 @@ class Scene2 extends Phaser.Scene{ //Here!!
 
 
    
-
+   //main function that updates the game
     update() {
         if(this.bonusShipActive)
             this.bonusship.x -= .75;
@@ -617,7 +510,9 @@ class Scene2 extends Phaser.Scene{ //Here!!
         }
 
     }
+    //player beam
     shootBeam(){
+        console.log(this.fireInterval);
        // var beam = this.physics.add.sprite(this.player.x, this.player.y, "beam");
        this.playerShootTime = false;
        if(this.doublePenLaser){
@@ -627,15 +522,10 @@ class Scene2 extends Phaser.Scene{ //Here!!
         this.powerUpActivite = false
         this.doublePenLaser = false;
     }
-        // console.log("projectiles shot : " + this.projectilesShot);
-        // console.log("double pen status : " + this.doublePenLaser);
-
         var beam = new Beam(this);
-       
-        //var beam = new Beam(this);
     }
 
-
+    //enemy beams 
     enemyShoot(){
         //checks for ships that are on the screen
         this.availableShip = [];
@@ -651,7 +541,7 @@ class Scene2 extends Phaser.Scene{ //Here!!
         this.enemy = this.availableShip[Math.floor(Math.random()*this.availableShip.length)];
         let newBeam = new BeamJr(this);
     }
-
+    //player movement
     movePlayerManager(){
         if(this.cursorKeys.left.isDown){
             this.player.setVelocityX(-gameSettings.playerSpeed);
@@ -661,17 +551,10 @@ class Scene2 extends Phaser.Scene{ //Here!!
         else{
             this.player.setVelocity(0);
         }
-        /*
-        if(this.cursorKeys.up.isDown){
-            this.player.setVelocityY(-gameSettings.playerSpeed);
-        } else if (this.cursorKeys.down.isDown){
-            this.player.setVelocityY(gameSettings.playerSpeed);
-        } */
-
     }
 
 
-
+    //moves the enemy ships off the board
     resetShipPos(ship, shipX){
         if(!this.shipGrave.includes(ship))
             this.shipGrave.push(ship);
@@ -682,7 +565,7 @@ class Scene2 extends Phaser.Scene{ //Here!!
         // console.log(ship.y);
         console.log("ship grave : " + this.shipGrave.length);
     }
-
+    //explosion animation
     destroyShip(pointer, gameObject) {
         try{gameObject.setTexture("explosion");
             gameObject.play("explode");}
